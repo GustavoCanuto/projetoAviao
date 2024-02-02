1563593eecf6eed94818fe93705de7d50a9c15a3
Gustavo Canuto <gcanuto@magnasistemas.com.br>
2024-02-02 10:42:01 -0300

    CRUD endereco

---
+++ apipassagem/src/main/java/br/com/magnasistemas/apipassagem/infra/exception/TratadorDeErros.java
@@ -0,0 +1,57 @@
+package br.com.magnasistemas.apipassagem.infra.exception;
+
+import java.util.List;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.magnasistemas.apipassagem.validacoes.ValidacaoException;
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratadorDeErros{
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<String> tratarErro404() {
+
+		return ResponseEntity.notFound().build();
+
+	}
+
+	@ExceptionHandler(ValidacaoException.class)
+	public ResponseEntity<String> tratarErroRegraDeNegocio(ValidacaoException ex) {
+
+		return ResponseEntity.badRequest().body(ex.getMessage());
+
+	}
+
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> tratarErro500(Exception ex) {
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro: " + ex.getLocalizedMessage());
+	}
+
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<String> tratarErroForeignKeyConstraint(DataIntegrityViolationException ex) {
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body("Não é possível excluir devido existir passagem registrada com essa entidade.");
+
+	}
+	
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<DadosErroValidacao>> tratarErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+    }
+    
+    private record DadosErroValidacao(String campo, String mensagem) {
+        public DadosErroValidacao(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
+    }
+}
