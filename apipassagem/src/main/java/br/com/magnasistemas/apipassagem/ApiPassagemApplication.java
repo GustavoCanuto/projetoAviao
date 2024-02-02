303f893f3c8faaed3127e0d08e281c46e68ab6f3
Gustavo Canuto <gcanuto@magnasistemas.com.br>
2024-02-02 06:43:59 -0300

    criado estrutura spring boot

---
+++ apipassagem/src/main/java/br/com/magnasistemas/apipassagem/ApiPassagemApplication.java
@@ -0,0 +1,14 @@
+package br.com.magnasistemas.apipassagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPassagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPassagemApplication.class, args);
+
+	}
+
+}
